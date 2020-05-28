d646246187d19aff6f9912779d903134caa7363e
Cosmin <minut1@yahoo.com>
2020-05-29 00:38:36 +0300

    Scale logo and search bar correctly

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -3,9 +3,9 @@ import Card from "@material-ui/core/Card";
 import logo from "../../res/logo.png";
 
 const cardStyle = {
-  width: "160px",
-  height: "40px",
-  padding: "5px",
+  width: "4.7504em",
+  height: "1.1876em",
+  padding: "8px",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",

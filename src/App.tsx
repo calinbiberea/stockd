e2ffa5fff651b1e7c688818b2b6e4fbc66d0ce0b
Nat Karmios <nk4118@ic.ac.uk>
2020-05-28 13:03:21 +0100

    Store clicked place ID in App state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import MapScreen from "./components/map/MapScreen";
 
-const App: React.FC = () => (
-  // eslint-disable-next-line no-console
-  <MapScreen onPlaceClick={(placeId) => console.log("Clicked on " + placeId)} />
-);
+const PlaceIdContext = React.createContext("");
+
+const App: React.FC = () => {
+  const [currentPlaceId, setCurrentPlaceId] = useState("");
+
+  const onPlaceClick = (placeId: string) => {
+    // eslint-disable-next-line no-console
+    console.log("Clicked on " + placeId);
+    setCurrentPlaceId(placeId);
+  };
+
+  return (
+    <div>
+      <MapScreen onPlaceClick={onPlaceClick} />
+    </div>
+  );
+};
 
 export default App;

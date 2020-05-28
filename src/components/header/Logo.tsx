9c4c69f7d2268fe08aa4d928f249872c5d53f6b0
Cosmin <minut1@yahoo.com>
2020-05-28 23:32:24 +0300

    Update Logo to use img, extract SearchArrow from Search, add functions for text control in Search, add divider in Header

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,24 +1,21 @@
-import logo from "../../res/logo.png";
-import Card from "@material-ui/core/Card";
 import React from "react";
+import Card from "@material-ui/core/Card";
+import logo from "../../res/logo.png";
 
 const cardStyle = {
   width: "160px",
   height: "40px",
-  background: "#FFF",
   padding: "5px",
 };
 
-const divStyle = {
+const imgStyle = {
   width: "100%",
   height: "100%",
-  backgroundImage: `url(${logo})`,
-  backgroundSize: "contain",
 };
 
 const Logo: React.FC = () => (
   <Card style={cardStyle}>
-    <div style={divStyle}>&nbsp;</div>
+    <img src={logo} style={imgStyle} alt="logo" />
   </Card>
 );
 
--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { IconButton, InputBase, makeStyles } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import ArrowIcon from "@material-ui/icons/ArrowForward";
-import colors from "../../res/colors";
 import shadows from "@material-ui/core/styles/shadows";
+import colors from "../../res/colors";
 
 const useStyles = makeStyles((theme) => ({
   search: {
@@ -43,28 +43,41 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+interface SearchArrowProps {
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const SearchArrow: React.FC<SearchArrowProps> = ({ onClick }: SearchArrowProps) => (
+  <div style={{ marginRight: "5px" }}>
+    <IconButton size="small" onClick={onClick}>
+      <ArrowIcon style={{ color: "#FFF" }} />
+    </IconButton>
+  </div>
+);
+
 const Search: React.FC = () => {
+  const [searchText, setSearchText] = useState("");
   const classes = useStyles();
+
+  const onClickSearch = () => console.warn("Search not implemented yet");
 
   return (
     <div className={classes.search} style={{ marginLeft: "auto" }}>
       <div className={classes.icon}>
         <SearchIcon />
       </div>
+
       <InputBase
+        type="text"
         placeholder="Search…"
+        value={searchText}
+        onChange={(event) => setSearchText(event.target.value)}
+        inputProps={{ "aria-label": "search" }}
+        endAdornment={<SearchArrow onClick={onClickSearch} />}
         classes={{
           root: classes.inputRoot,
           input: classes.inputInput,
         }}
-        inputProps={{ "aria-label": "search" }}
-        endAdornment={
-          <div style={{ marginRight: "5px" }}>
-            <IconButton size="small">
-              <ArrowIcon style={{ color: "#FFF" }} />
-            </IconButton>
-          </div>
-        }
       />
     </div>
   );

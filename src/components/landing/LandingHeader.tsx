33d99f5db945f57369361410293301db8ba826ba
Nat Karmios <nk4118@ic.ac.uk>
2020-06-02 00:37:36 +0100

    Add LandingHeader component

---
+++ src/components/landing/LandingHeader.tsx
@@ -0,0 +1,42 @@
+import { Card, Typography } from "@material-ui/core";
+import colors from "../../res/colors";
+import Logo from "../../res/logo.png";
+import React from "react";
+
+const containerStyle = {
+  width: "100%",
+  position: "relative" as const,
+  padding: "20px 0",
+};
+
+const cardStyle = {
+  position: "relative" as const,
+  display: "inline-block",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  textAlign: "center" as const,
+  padding: "10px",
+};
+
+const welcomeStyle = {
+  fontSize: "1.5vh",
+  color: colors.blue1,
+};
+
+const logoStyle = {
+  maxHeight: "4vh",
+};
+
+const LandingHeader: React.FC = () => (
+  <div style={containerStyle}>
+    <Card style={cardStyle}>
+      <Typography variant="h6" style={welcomeStyle}>
+        Welcome to
+      </Typography>
+      <img style={logoStyle} src={Logo} alt="stockd logo" />
+    </Card>
+  </div>
+);
+
+export default LandingHeader;

ce38086050d0b37e7531a993307d9809cd8ef91f
Cosmin <minut1@yahoo.com>
2020-06-01 22:49:58 +0300

    Fix styling in Splash

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -2,17 +2,14 @@ import React, { useEffect, useState } from "react";
 import { Fade } from "@material-ui/core";
 import logo from "../res/logo.png";
 
-const fadeStyle = {
-  position: "absolute" as const,
-  zIndex: 2000,
-  width: "100vw",
-};
-
 const logoContainerStyle = {
+  position: "absolute" as const,
+  height: "100vh",
+  width: "100vw",
+  zIndex: 2000,
+  display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  display: "flex",
-  height: "100vh",
   backgroundColor: "white",
 };
 
@@ -28,7 +25,7 @@ const Splash: React.FC = () => {
   }, []);
 
   return (
-    <Fade in={!loaded} style={fadeStyle} timeout={{ enter: 0, exit: 1000 }}>
+    <Fade in={!loaded} timeout={{ enter: 0, exit: 1000 }}>
       <div style={logoContainerStyle}>
         <img src={logo} alt="logo" style={logoStyle} />
       </div>

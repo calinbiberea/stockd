c8f39f6b23885cc5ba5620ded89d82f2f6d2f962
Cosmin <minut1@yahoo.com>
2020-06-05 16:02:35 +0300

    Add button on Shop Overview that takes users to a google maps page of the shops, allowing them to request directions

--- src/components/shop/ShopOverview.tsx
+++ src/components/shop/ShopOverview.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import StockItem from "./StockItem";
 import { ShopOverviewProps } from "./ShopTypes";
@@ -23,18 +24,31 @@ const gridItemStyle = {
   width: "50%",
 };
 
-const ShopOverview: React.FC<ShopOverviewProps> = ({ stocks }: ShopOverviewProps) => {
+const ShopOverview: React.FC<ShopOverviewProps> = ({ stocks, locationData }: ShopOverviewProps) => {
   const stockItems = Object.entries(stocks).map(([name, { icon, stock }]) => (
     <Grid item key={name} style={gridItemStyle}>
       <StockItem icon={icon} name={name} stock={stock} />
     </Grid>
   ));
 
+  const onButtonClick = () => {
+    const getMapsUrl = (placeName: string, placeId: string) =>
+      `https://www.google.com/maps/search/?api=1&query=${encodeURI(
+        placeName
+      )}&query_place_id=${placeId}`;
+
+    window.open(getMapsUrl(locationData.name, locationData.id));
+  };
+
   return (
     <div style={containerStyle}>
       <Grid container style={gridContainerStyle}>
         {stockItems}
       </Grid>
+
+      <Button variant="contained" color="primary" onClick={onButtonClick}>
+        Take me there
+      </Button>
     </div>
   );
 };

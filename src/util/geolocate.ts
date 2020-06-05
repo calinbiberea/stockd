ca7e94d68d7dfc4628b39feed6cd90fb65e525b2
Nat Karmios <nk4118@ic.ac.uk>
2020-06-05 01:38:33 +0100

    Add notifications for geolocation to FilterShops

--- src/util/geolocate.ts
+++ src/util/geolocate.ts
@@ -1,4 +1,21 @@
-export const getCurrentLocation = (): Promise<Position> =>
+import { OptionsObject, SnackbarKey, SnackbarMessage } from "notistack";
+
+type NotifyFunc = (message: SnackbarMessage, options?: OptionsObject) => SnackbarKey;
+
+export const getCurrentLocation = (notify?: NotifyFunc): Promise<Position | null> =>
   new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        if (notify !== undefined) {
+          notify("Successfully got your location", { variant: "success" });
+        }
+        resolve(position);
+      },
+      () => {
+        if (notify !== undefined) {
+          notify("Failed to get your location", { variant: "error" });
+        }
+        resolve(null);
+      }
+    );
   });

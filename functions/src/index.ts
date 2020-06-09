3ebe375bab1c6a4de7a9a9015ce5525f30a95d1b
Nat Karmios <nk4118@ic.ac.uk>
2020-06-09 02:04:37 +0100

    Register new cloud funcs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,12 @@
 import * as admin from "firebase-admin";
 import findShops from "./findShops";
+import { pushStockUpdate } from "./stockUpdates/pushStockUpdate";
+import { onStockUpdate } from "./stockUpdates/onStockUpdate";
 
 admin.initializeApp();
 
 module.exports = {
   findShops,
+  pushStockUpdate,
+  onStockUpdate,
 };

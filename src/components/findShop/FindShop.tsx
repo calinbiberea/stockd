ea3791d567f720bc28eacf3adc64787d7ad2b6de
Cosmin <cb3618@ic.ac.uk>
2020-06-10 18:20:39 +0300

    Fix styling for FindShop to match FilterShops

--- src/components/findShop/FindShop.tsx
+++ src/components/findShop/FindShop.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import {
   Button,
   Checkbox,
-  createStyles,
   Divider,
   FormControl,
   FormControlLabel,
@@ -12,15 +11,16 @@ import {
   Select,
   TextField,
   Typography,
+  makeStyles,
+  createStyles,
 } from "@material-ui/core";
 import ArrowIcon from "@material-ui/icons/ArrowForward";
+import { useSnackbar } from "notistack";
+import { FindShopProps } from "./FindShopTypes";
 import Header from "../header/Header";
 import LocationSearch from "../filterShops/LocationSearch";
 import ShopList from "../shopList/ShopList";
 import { getCurrentLocation } from "../../util/geolocate";
-import { useSnackbar } from "notistack";
-import { makeStyles } from "@material-ui/core/styles";
-import { FindShopProps } from "./FindShopTypes";
 
 type AutocompletePrediction = google.maps.places.AutocompletePrediction;
 
@@ -30,29 +30,45 @@ const useStyles = makeStyles((theme) =>
       width: "100vw",
       height: "100vh",
       display: "flex",
-      flexDirection: "column" as const,
+      flexDirection: "column",
       alignItems: "center",
-      overflowY: "scroll",
+      overflowY: "auto",
     },
     title: {
-      margin: "20px",
+      padding: "20px",
     },
     contentContainer: {
       margin: "20px",
       display: "flex",
-      flexDirection: "column" as const,
+      flexDirection: "column",
       alignItems: "center",
+    },
+    locationContainer: {
+      marginTop: "8px",
+      alignItems: "center",
+      [theme.breakpoints.up("sm")]: {
+        flexDirection: "row",
+      },
+      [theme.breakpoints.down("xs")]: {
+        flexDirection: "column",
+      },
     },
     distanceSelect: {
       margin: theme.spacing(1),
       minWidth: 120,
     },
-    submitDivider: {
+    locationDivider: {
       margin: `0px ${theme.spacing(2)}px`,
+      [theme.breakpoints.down("xs")]: {
+        visibility: "hidden",
+      },
     },
     button: {
-      margin: "auto 20px",
-      flex: "0",
+      margin: "auto 20px 16px",
+      flex: 0,
+    },
+    buttonIcon: {
+      marginLeft: "16px",
     },
   })
 );
@@ -64,24 +80,30 @@ const FindShop: React.FC<FindShopProps> = ({ setRoute }: FindShopProps) => {
   const [useCurrentLocation, setUseCurrentLocation] = useState(false);
   const [currentLocation, setCurrentLocation] = useState<Position | null>(null);
   const [submitted, setSubmitted] = useState(false);
+
   const classes = useStyles();
+
   const { enqueueSnackbar } = useSnackbar();
 
   const toggleGeolocation = async (enabled: boolean) => {
     let newUseCurrentLocation = enabled;
     if (enabled) {
       const maybeCurrentLocation = await getCurrentLocation(enqueueSnackbar);
+
       if (maybeCurrentLocation === null) {
         newUseCurrentLocation = false;
       }
+
       setCurrentLocation(maybeCurrentLocation);
     }
+
     setUseCurrentLocation(newUseCurrentLocation);
   };
 
   const getLocation = () => {
     if (useCurrentLocation) {
       const coords = (currentLocation as Position).coords;
+
       return {
         geolocated: true as const,
         lat: coords.latitude,
@@ -117,65 +139,68 @@ const FindShop: React.FC<FindShopProps> = ({ setRoute }: FindShopProps) => {
     <div className={classes.container}>
       <Header onBackClick={() => setRoute("landing")} />
 
-      <Typography className={classes.title} variant="h4" color="primary">
+      <Typography variant="h4" color="primary" className={classes.title}>
         Which shop are you looking for?
       </Typography>
 
-      <div>
+      <div className={classes.contentContainer}>
         <TextField
           required
-          id="outlined-required"
           label="Enter shop name"
           variant="outlined"
           defaultValue={shopName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setShopName(e.target.value)}
+          onChange={(event) => setShopName(event.target.value)}
         />
 
-        <div className={classes.contentContainer}>
-          <FormGroup row>
-            <FormControl className={classes.distanceSelect}>
-              <InputLabel>Max. distance</InputLabel>
-              <Select
-                value={maxDistance}
-                onChange={(e) => setMaxDistance(parseInt(e.target.value as string))}
-              >
-                <MenuItem value={10}>10km</MenuItem>
-                <MenuItem value={25}>25km</MenuItem>
-                <MenuItem value={50}>50km</MenuItem>
-              </Select>
-            </FormControl>
-            <Divider className={classes.submitDivider} orientation="vertical" flexItem />
-            <LocationSearch
-              enabled={!useCurrentLocation}
-              location={selectedPlace}
-              setLocation={setSelectedPlace}
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  color="primary"
-                  checked={useCurrentLocation}
-                  onChange={(event) => toggleGeolocation(event.target.checked)}
-                />
-              }
-              label="Use current location"
-            />
-          </FormGroup>
-        </div>
+        <FormGroup row className={classes.locationContainer}>
+          <FormControl className={classes.distanceSelect}>
+            <InputLabel>Max. distance</InputLabel>
 
-        <Button
-          size="large"
-          color="primary"
-          variant="contained"
-          onClick={() => setSubmitted(true)}
-          className={classes.button}
-          disabled={!canSubmit}
-        >
-          <Typography variant="h6">{"Let's go!"}</Typography>
+            <Select
+              value={maxDistance}
+              onChange={(e) => setMaxDistance(parseInt(e.target.value as string))}
+            >
+              <MenuItem value={10}>10km</MenuItem>
 
-          <ArrowIcon style={{ marginLeft: "16px" }} />
-        </Button>
+              <MenuItem value={25}>25km</MenuItem>
+
+              <MenuItem value={50}>50km</MenuItem>
+            </Select>
+          </FormControl>
+
+          <Divider orientation="vertical" flexItem className={classes.locationDivider} />
+
+          <LocationSearch
+            enabled={!useCurrentLocation}
+            location={selectedPlace}
+            setLocation={setSelectedPlace}
+          />
+
+          <FormControlLabel
+            label="Use current location"
+            control={
+              <Checkbox
+                color="primary"
+                checked={useCurrentLocation}
+                onChange={(event) => toggleGeolocation(event.target.checked)}
+              />
+            }
+          />
+        </FormGroup>
       </div>
+
+      <Button
+        size="large"
+        color="primary"
+        variant="contained"
+        disabled={!canSubmit}
+        onClick={() => setSubmitted(true)}
+        className={classes.button}
+      >
+        <Typography variant="h6">{"Let's go!"}</Typography>
+
+        <ArrowIcon className={classes.buttonIcon} />
+      </Button>
     </div>
   );
 };
